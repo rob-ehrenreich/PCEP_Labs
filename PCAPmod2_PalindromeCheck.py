360f330f189ffe8db09610f3a33a6d9316b1f881
kodename <kodename@users.noreply.github.com>
2025-06-18 17:06:07 -0700

    Create PCAPmod2_PalindromeCheck.py

---
+++ PCAPmod2_PalindromeCheck.py
@@ -0,0 +1,25 @@
+"""
+Ask user for text, check whether entered text is a palindrome.
+Don't include spaces in the check and an empty string is a not a plaindrome
+"""
+pal = "It's a palindrome"
+npal = "It's not a palindrome"
+
+def reverse_string(x):
+    return x[::-1]
+
+def palcheck(txtstring):
+    s = txtstring.replace(" ","")
+    r = reverse_string(s)
+    if s.lower() == r.lower():
+        print(pal)
+    else:
+        print(npal)
+    return
+
+userin = input("Enter string to test: ")
+
+if userin == "" or userin.replace(" ","") == "":
+    print(npal)
+else:
+    palcheck(userin)
